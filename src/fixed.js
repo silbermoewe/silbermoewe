26d4f1b8e0688184cb594911a003f4007d5bd7a4
k-lange <mail@klange.org>
2015-02-16 22:59:46 +0100

    use requestAnimationFrame again, don't use jQuery for fixed css

--- src/fixed.js
+++ src/fixed.js
@@ -4,7 +4,9 @@ var $ = require('jquery'),
 var $articles = $('article'),
 	fixedEls = [],
 	lastScrollTop = 0,
-	ticking = false,
+	scrolling,
+	debouncedCancelMoveNav = _.debounce(cancelMoveNav, 500),
+	transform = document.body.style.transform ? 'transform' : 'webkitTransform',
 	windowHeight;
 
 function cacheOffsets() {
@@ -18,6 +20,18 @@ function cacheOffsets() {
 }
 
 function onScroll() {
+	debouncedCancelMoveNav();
+
+	if (scrolling) { return; }
+
+	onTick();
+}
+
+function cancelMoveNav() {
+	scrolling = false;
+}
+
+function onTick() {
 	var scrollTop = window.pageYOffset;
 	scrollTop = (scrollTop < 0) ? 0 : scrollTop;
 
@@ -27,15 +41,14 @@ function onScroll() {
 
 	lastScrollTop = scrollTop;
 
-	if (!ticking) {
-		ticking = true;
-		moveNav();
-	}
+	moveNav();
+
+	if (!scrolling) { return; }
+
+	window.requestAnimationFrame(onTick);
 }
 
 function moveNav() {
-	ticking = false;
-
 	var inViewport = [];
 
 	for (var i = fixedEls.length - 1; i >= 0; i--) {
@@ -50,7 +63,7 @@ function moveNav() {
 	}
 
 	inViewport.forEach(function (el) {
-		el.fixed.css('transform', 'translate3d(0,' + (lastScrollTop - el.offset) + 'px,0)');
+		el.fixed[0].style[transform] = 'translate3d(0,' + (lastScrollTop - el.offset) + 'px,0)';
 	});
 }
 
