f92e00e989d612f6ec1db833745a322eb94902c6
k-lange <mail@klange.org>
2015-08-22 13:55:48 +0100

    feat(get-image-url): calculate image size base on breakpoints

--- src/get-image-url.js
+++ src/get-image-url.js
@@ -1,11 +1,24 @@
 var _ = require('lodash');
 var win = require('./window-dimensions');
 var sizes = [400, 800, 1200, 1600, 2000];
+var breakPoints = [
+    {
+        size: 600,
+        ratio: 1
+    },
+    {
+        size: 900,
+        ratio: 0.75
+    },
+    {
+        ratio: 0.375
+    }
+];
 
 module.exports = getUrl;
 
 function getCurrentSize(full) {
-    var width = full ? win.width : (win.width  * 0.375 - 40);
+    var width = full ? win.width : (getContentWidth() - 40);
     return _.find(sizes, function (size) {
         return width * window.devicePixelRatio < size;
     }) || _.last(sizes);
@@ -14,4 +27,12 @@ function getCurrentSize(full) {
 function getUrl(image) {
     var suffix = image.background ? '' : ('-' + getCurrentSize(image.full) + '.jpg');
     return image.path + suffix;
+}
+
+function getContentWidth() {
+    var breakPoint =  _.find(breakPoints, function (breakPoint) {
+        return !breakPoint.size || breakPoint.size > win.width;
+    });
+
+    return win.width * breakPoint.ratio;
 }
