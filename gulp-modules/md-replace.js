a0ae9ce3d215caa0a89b74da024797a2c9c142db
Kolja Lange <mail@klange.org>
2024-06-09 17:40:24 +0200

    refactor: md-replace, remove bluebird, const

--- gulp-modules/md-replace.js
+++ gulp-modules/md-replace.js
@@ -1,24 +1,23 @@
-var es = require('event-stream'),
-    $ = require('cheerio'),
-    _ = require('lodash'),
-    Promise = require('bluebird'),
-    Hypher = require('hypher'),
-    color = Promise.promisify(require('dominant-color')),
-    hypherEn = new Hypher(require('hyphenation.en-us')),
-    hypherDe = new Hypher(require('hyphenation.de'));
+const es = require('event-stream');
+const $ = require('cheerio');
+const _ = require('lodash');
+const Hypher = require('hypher');
+const { promisify } = require('util');
+const dominantColor = promisify(require('dominant-color'));
+const hypherEn = new Hypher(require('hyphenation.en-us'));
+const hypherDe = new Hypher(require('hyphenation.de'));
 
-var config = require('../config.json');
+const config = require('../config.json');
 
 module.exports = function () {
     return es.map(mdReplace);
 };
 
 function mdReplace(file, callback) {
-    var postsObject = JSON.parse(file.contents.toString());
-    var posts = _.map(postsObject, mapPost);
+    const postsObject = JSON.parse(file.contents.toString());
+    const posts = _.map(postsObject, mapPost);
 
-    Promise.resolve(posts)
-        .map(handlePost)
+    Promise.all(posts.map(handlePost))
         .then(sort)
         .then(function (posts) {
             file.contents = new Buffer(JSON.stringify(posts));
@@ -38,7 +37,7 @@ function handlePost(post) {
 }
 
 function getDominantColor(post) {
-    return color(config.posts + '/' + post.backgroundImage)
+    return dominantColor(config.posts + '/' + post.backgroundImage)
         .then(function (color) {
             post.backgroundColor = '#' + color;
             return post;
@@ -50,10 +49,10 @@ function getDominantColor(post) {
 }
 
 function replaceImages(post) {
-    var $body = $('<span>' + post.body + '</span>');
+    const $body = $('<span>' + post.body + '</span>');
 
     $body.find('img').each(function () {
-        var $image = $('<div/>')
+        const $image = $('<div/>')
             .addClass('image')
             .attr('data-src', 'pictures/' + post.folder + '/' + $(this).attr('src'));
 
@@ -74,7 +73,7 @@ function replaceImages(post) {
 }
 
 function splitLanguages(post) {
-    var languages = post.body.split('<p>---en---</p>\n');
+    const languages = post.body.split('<p>---en---</p>\n');
     post.body_de = hyphenate(languages[0], hypherDe);
     post.body_en = hyphenate(languages[1], hypherEn);
 
@@ -82,7 +81,7 @@ function splitLanguages(post) {
 }
 
 function hyphenate(string, hyphenator) {
-    var $body = $('<span>' + string + '</span>');
+    const $body = $('<span>' + string + '</span>');
     $body.find('p').each(function () {
         _.forEach(this.childNodes, function (node) {
             if (node.nodeType === 3) {
@@ -95,15 +94,15 @@ function hyphenate(string, hyphenator) {
 }
 
 function createPreview(post) {
-    var previewContainer = $('<div>' + post.body_en + '</div>').find('p');
+    const previewContainer = $('<div>' + post.body_en + '</div>').find('p');
     post.preview = previewContainer.eq(0).text() + previewContainer.eq(1).text();
 
     return post;
 }
 
 function sort(posts) {
-    var sortedPosts = _.sortBy(posts, 'date').reverse();
-    var startKm = _.last(sortedPosts).km;
+    const sortedPosts = _.sortBy(posts, 'date').reverse();
+    const startKm = _.last(sortedPosts).km;
 
     sortedPosts.forEach(function (post) {
         post.days = Math.round((post.date - _.last(sortedPosts).date) / (1000 * 60 * 60 * 24));
